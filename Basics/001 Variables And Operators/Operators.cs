91b67999fc47839869851aa03339742f70cb97f6
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 10:48:14 -0700

    Added comments to explain the different data types for variables.

--- Basics/001 Variables And Operators/Operators.cs
+++ Basics/001 Variables And Operators/Operators.cs
@@ -8,11 +8,11 @@ public class Program
 	public static void Main()
 	{
 		//variables:
-		int a = 10;
+		int a = 10; //an "int" is an integer. They are used if you want a whole number.
 		int b = 5;
-		float c = 1.2F;
-		double d = 2.555D;
-		string complaint = "I had to redo this section lol.";
+		float c = 1.2F; //a float is a number with a decimal value. They are used if you want a precise number value.
+		double d = 2.555D; //a double is a decimal value number, but it can hole more data than a float.
+		string complaint = "I had to redo this section lol."; //a string is a value that has a line of text. It can be used to communicate to the user and/or terminal.
 		
 		//writing the variables in the terminal:
 		Console.WriteLine(a);
@@ -21,7 +21,7 @@ public class Program
 		Console.WriteLine(d);
 		Console.WriteLine(complaint);
 		
-		Console.WriteLine("");
+		Console.WriteLine(""); //just using this to separate variables from operators.
 		
 		//modifying these variables with operators:
 		Console.WriteLine(a + b); //you can modify these variables directly within a WriteLine command,
