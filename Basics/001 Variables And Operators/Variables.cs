8ca1901c4feef7b41924cab012e91cc18d90098b
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 11:04:27 -0700

    Fixed a typo that was present in both files.

--- Basics/001 Variables And Operators/Variables.cs
+++ Basics/001 Variables And Operators/Variables.cs
@@ -8,7 +8,7 @@ public class Program
 		int a = 10; //an "int" is an integer. They are used if you want a whole number.
 		int b = 5;
 		float c = 1.2F; //a float is a number with a decimal value. They are used if you want a precise number value.
-		double d = 2.555D; //a double is a decimal value number, but it can hole more data than a float.
+		double d = 2.555D; //a double is a decimal value number, but it can hold more data than a float.
 		string complaint = "I had to redo this section lol."; //a string is a value that has a line of text. It can be used to communicate to the user and/or terminal.
 		
 		//writing the variables in the terminal:
