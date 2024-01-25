9c99a9139e5483fb8dfea4b29158c371b60f7d39
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 15:13:48 -0700

    Added a fourth hero to Program class.

--- Basics/001 Conventions and Syntax/HeroClasses.cs
+++ Basics/001 Conventions and Syntax/HeroClasses.cs
@@ -7,12 +7,14 @@ public class Program
 	public Hero heroOne;
 	public Hero heroTwo;
 	public Hero heroThree;
+	public Hero heroFour;
 	
 	public void Main()
 	{
 		heroOne = new Hero();
 		heroTwo = new Hero();
 		heroThree = new Hero();
+		heroFour = new Hero();
 		
 		heroOne.health = 2;
 		heroOne.powerLevel = 5;
@@ -28,21 +30,34 @@ public class Program
 		heroThree.powerLevel = 10;
 		heroThree.defense = 3;
 		heroThree.height = 2.0F;
+
+		heroFour.health = 3;
+		heroFour.powerLevel = 9001;
+		heroFour.defense = 8;
+		heroFour.height = 3.0F;
 		
 		Console.WriteLine(heroOne.health);
 		Console.WriteLine(heroOne.powerLevel);
 		Console.WriteLine(heroOne.defense);
 		Console.WriteLine(heroOne.height);
 		
+		Console.WriteLine("");
 		Console.WriteLine(heroTwo.health);
 		Console.WriteLine(heroTwo.powerLevel);
 		Console.WriteLine(heroTwo.defense);
 		Console.WriteLine(heroTwo.height);
 
+		Console.WriteLine("");
 		Console.WriteLine(heroThree.health);
 		Console.WriteLine(heroThree.powerLevel);
 		Console.WriteLine(heroThree.defense);
 		Console.WriteLine(heroThree.height);
+
+		Console.WriteLine("");
+		Console.WriteLine(heroFour.health);
+		Console.WriteLine(heroFour.powerLevel);
+		Console.WriteLine(heroFour.defense);
+		Console.WriteLine(heroFour.height);
 	}
 }
 
