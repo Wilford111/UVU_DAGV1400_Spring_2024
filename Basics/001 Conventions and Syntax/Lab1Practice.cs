f35904767887aa7b9c9274c04663f13fb473ca33
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 12:16:41 -0700

    Replaced name and added a variable and operation

--- Basics/001 Conventions and Syntax/Lab1Practice.cs
+++ Basics/001 Conventions and Syntax/Lab1Practice.cs
@@ -6,10 +6,13 @@ namespace MyApplication
   {
     static void Main(string[] args)
     {
-      string name = "Anthony";
+      string name = "Wilford";
       Console.WriteLine(name); 
       int number = 11;
       Console.WriteLine(number);
+      int numberB = 15;
+      Console.WriteLine(numberB);
+      Console.WriteLine(number + numberB);
      }
   }
 }
