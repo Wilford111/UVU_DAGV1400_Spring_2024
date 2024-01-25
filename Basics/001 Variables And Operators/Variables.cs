25a781bca0b8f0be31bee8d780124f7afe378afa
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 10:51:27 -0700

    Added comments in variables.cs to make it more in line with operators.cs, and to more closely follow assignment directions.

--- Basics/001 Variables And Operators/Variables.cs
+++ Basics/001 Variables And Operators/Variables.cs
@@ -4,12 +4,14 @@ public class Program
 {
 	public static void Main()
 	{
-		int a = 10;
+        //variables:
+		int a = 10; //an "int" is an integer. They are used if you want a whole number.
 		int b = 5;
-		float c = 1.2F;
-		double d = 2.555D;
-		string complaint = "I had to redo this section lol.";
+		float c = 1.2F; //a float is a number with a decimal value. They are used if you want a precise number value.
+		double d = 2.555D; //a double is a decimal value number, but it can hole more data than a float.
+		string complaint = "I had to redo this section lol."; //a string is a value that has a line of text. It can be used to communicate to the user and/or terminal.
 		
+		//writing the variables in the terminal:
 		Console.WriteLine(a);
 		Console.WriteLine(b);
 		Console.WriteLine(c);
