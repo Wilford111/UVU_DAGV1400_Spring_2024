7215e1c94c163f5a6d0a93d82102e988e94eaf47
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 14:17:56 -0700

    Added new variables to Hero class. Added new heroThree variable to Program.

--- Basics/001 Conventions and Syntax/HeroClasses.cs
+++ Basics/001 Conventions and Syntax/HeroClasses.cs
@@ -6,27 +6,49 @@ public class Program
 {
 	public Hero heroOne;
 	public Hero heroTwo;
+	public Hero heroThree;
 	
 	public void Main()
 	{
 		heroOne = new Hero();
 		heroTwo = new Hero();
+		heroThree = new Hero();
 		
 		heroOne.health = 2;
 		heroOne.powerLevel = 5;
+		heroOne.defense = 5;
+		heroOne.height = 1.83F;
 		
 		heroTwo.health = 3;
 		heroTwo.powerLevel = 1;
+		heroTwo.defense = 8;
+		heroTwo.height = 1.65F;
+
+		heroThree.health = 2;
+		heroThree.powerLevel = 10;
+		heroThree.defense = 3;
+		heroThree.height = 2.0F;
 		
 		Console.WriteLine(heroOne.health);
 		Console.WriteLine(heroOne.powerLevel);
+		Console.WriteLine(heroOne.defense);
+		Console.WriteLine(heroOne.height);
 		
 		Console.WriteLine(heroTwo.health);
 		Console.WriteLine(heroTwo.powerLevel);
+		Console.WriteLine(heroTwo.defense);
+		Console.WriteLine(heroTwo.height);
+
+		Console.WriteLine(heroThree.health);
+		Console.WriteLine(heroThree.powerLevel);
+		Console.WriteLine(heroThree.defense);
+		Console.WriteLine(heroThree.height);
 	}
 }
 
 public class Hero {
 	public int health;
 	public int powerLevel;
+	public int defense;
+	public float height;
 }
