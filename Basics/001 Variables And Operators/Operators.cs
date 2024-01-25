746e81cc648409a9f89e378f9a08aceaa5f025aa
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 11:10:10 -0700

    Added 'character' data type example to both files.

--- Basics/001 Variables And Operators/Operators.cs
+++ Basics/001 Variables And Operators/Operators.cs
@@ -13,13 +13,15 @@ public class Program
 		float c = 1.2F; //a float is a number with a decimal value. They are used if you want a precise number value.
 		double d = 2.555D; //a double is a decimal value number, but it can hold more data than a float.
 		string complaint = "I had to redo this section lol."; //a string is a value that has a line of text. It can be used to communicate to the user and/or terminal.
-		
+		char izard = 'C'; //a character is a value that only contains one character. It can be a letter or a number.
+
 		//writing the variables in the terminal:
 		Console.WriteLine(a);
 		Console.WriteLine(b);
 		Console.WriteLine(c);
 		Console.WriteLine(d);
 		Console.WriteLine(complaint);
+        Console.WriteLine(izard);
 		
 		Console.WriteLine(""); //just using this WriteLine function to separate variables from operators.
 		
