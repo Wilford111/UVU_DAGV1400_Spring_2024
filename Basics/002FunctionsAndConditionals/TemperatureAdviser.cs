d682987ce8f0d8afff04f004de72ce39a16fbdc2
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-01-25 23:07:02 -0700

    Cleaned up my code

--- Basics/002FunctionsAndConditionals/TemperatureAdviser.cs
+++ Basics/002FunctionsAndConditionals/TemperatureAdviser.cs
@@ -2,23 +2,12 @@ using System;
 					
 public class Program
 {
-	public StoredVariable openedVariable;
-	
 	public void Main()
 	{
-		openedVariable = new StoredVariable();
-		
 		Console.WriteLine("Please enter the current temperature in Celsius: ");
-		openedVariable.tempInt = Console.ReadLine();
+		string tempInt = Console.ReadLine();
 		
-		int temp = Int32.Parse(openedVariable.tempInt);
-		
-//		try
-//		{
-//			int temp = Int32.Parse(openedVariable.tempInt);
-//		} catch (FormatException) {
-//			Console.WriteLine("Please enter a numeric value.");
-//		}
+		int temp = Int32.Parse(tempInt);
 		
 		if(temp > 30){
 			Console.WriteLine("Please stay hydrated and avoid staying in the sun for too long!");
@@ -26,10 +15,4 @@ public class Program
 			Console.WriteLine("Go out and enjoy the pleasant weather!");
 		}
 	}
-}
-
-public class StoredVariable
-{
-	public string tempInt; 
-	
 }
