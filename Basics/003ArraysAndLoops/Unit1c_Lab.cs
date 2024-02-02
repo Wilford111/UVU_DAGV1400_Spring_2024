e8cc29b66df92e9d34ea10ab89d4359577b3c77f
Wilford111 <92828408+Wilford111@users.noreply.github.com>
2024-02-01 19:36:55 -0700

    Added comments

--- Basics/003ArraysAndLoops/Unit1c_Lab.cs
+++ Basics/003ArraysAndLoops/Unit1c_Lab.cs
@@ -9,10 +9,15 @@ public class Program
 		int column = 1;
 		
 		for(var i = 1;input >= i; i++){
+            //for every time input is greater than or equal to i...
 			for(var c = 1;column >= c; c++)
+                //for every time column is greater than or equal to c...
 				Console.Write(column);
+                //print the value of column
 				column++;
+                //increase the value of column by 1
 			Console.WriteLine("");
+            //print a new line
 		}
 	}
 }
