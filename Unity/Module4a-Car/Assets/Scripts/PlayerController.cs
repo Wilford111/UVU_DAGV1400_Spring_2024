2b01fa2ac0028b2c21a0f65b966555a331d9bc02
11053051@uvu.edu <11053051@uvu.edu>
2024-02-20 14:52:57 -0700

    Completed Car prototype

--- Unity/Module4a-Car/Assets/Scripts/PlayerController.cs
+++ Unity/Module4a-Car/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Private variables
+    private float speed = 16.0f;
+    private float turnSpeed = 60.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Allow the vehicle to move based on player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotate the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
