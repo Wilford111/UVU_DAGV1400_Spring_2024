9e6c163f6d07918239d738eac64bc1bff45fc951
11053051@uvu.edu <11053051@uvu.edu>
2024-02-27 15:09:02 -0700

    Completed Module 4b challenge

---
+++ Unity/Module4b/Module-4b-Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnDelay = 0.4f;
+    public float timer = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // increase timer by deltaTime
+        if(timer < 2){
+            timer += Time.deltaTime;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= spawnDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            // Reset timer to 0
+            timer = 0;
+        }
+    }
+}
