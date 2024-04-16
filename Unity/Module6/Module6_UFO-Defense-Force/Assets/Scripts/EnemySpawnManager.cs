f551e04d4a1eb1e02960bfee4c63eec6990f1286
Wilford111 <11053051@uvu.edu>
2024-04-16 09:04:40 -0600

    Completed Module 6b Lab Assignment

--- Unity/Module6/Module6_UFO-Defense-Force/Assets/Scripts/EnemySpawnManager.cs
+++ Unity/Module6/Module6_UFO-Defense-Force/Assets/Scripts/EnemySpawnManager.cs
@@ -7,15 +7,24 @@ public class EnemySpawnManager : MonoBehaviour
     public GameObject[] ufoPrefabs; //Array to store UFO ships
     private float spawnRangeX = 23f;
     private float spawnPosZ = 25f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); //Creates a random spawn position on the X axis
-            int ufoIndex = Random.Range(0, ufoPrefabs.Length); //Picks a random UFO from the array
-            Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //Spawns indexed UFO from the array
-        }
+        
+    }
+
+    void SpawnRandomUFO()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ); //Creates a random spawn position on the X axis
+        int ufoIndex = Random.Range(0, ufoPrefabs.Length); //Picks a random UFO from the array
+        Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //Spawns indexed UFO from the array
     }
 }
